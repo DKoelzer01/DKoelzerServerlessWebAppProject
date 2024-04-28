1eaec1e1ab4efecb74bc1dec633ddb1ef56360c6
Daniel Koelzer <DKoelzer01@gmail.com>
2024-04-27 23:27:35 -0500

    Update ride.js

--- js/ride.js
+++ js/ride.js
@@ -93,11 +93,11 @@ let map;
                 maxZoom: 19,
                 attribution: '© OpenStreetMap'
             }).addTo(map);
-            L.tileLayer('https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid={API key}', {
+            L.tileLayer('https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid={apikey}', {
                 maxZoom: 19,
                 opacity: .3,
                 layer: 'precipitation_new',
-                appid: '1722f41feef903a63d213e88e34b0974',
+                apikey: '1722f41feef903a63d213e88e34b0974',
                 attribution: '© OpenWeatherMap'
             }).addTo(map);
 
