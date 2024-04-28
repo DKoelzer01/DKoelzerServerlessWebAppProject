7f3254ca8435d34a7aae894b3af06dcb715a8a28
Daniel Koelzer <DKoelzer01@gmail.com>
2024-04-27 23:08:27 -0500

    Update ride.js

--- js/ride.js
+++ js/ride.js
@@ -89,8 +89,13 @@ let map;
 
         function setLocation(loc) {
             map = L.map('map').setView([loc.coords.latitude, loc.coords.longitude], 13);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '© OpenStreetMap'
+            }).addTo(map);
             L.tileLayer('https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=1722f41feef903a63d213e88e34b0974', {
                 maxZoom: 19,
+                opacity: .3,
                 attribution: '© OpenWeatherMap'
             }).addTo(map);
 
